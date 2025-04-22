918b6d0f3a2aca5896fb442b7e8b37b46649a9f4
kalpesh0487 <desaikalpesh2003@gmail.com>
2025-04-22 16:26:39 +0530

    added backend call

--- src/store/dataStore.ts
+++ src/store/dataStore.ts
@@ -1,6 +1,28 @@
+//import { create } from "zustand";
+//import { NetworkData } from "../types/types";
+//import networkData from "../data/networkData";
+
+//interface NetworkState {
+//    networkData: NetworkData;
+//    setNetworkData: (data: NetworkData) => void;
+//}
+
+//export const useNetworkStore = create<NetworkState>((set) => ({
+//    networkData: {
+//      nodes: [],
+//      connections: [],
+//      groups: []
+//    },
+//    setNetworkData: (data: NetworkData) => set({ networkData: data }),
+// }));
+// console.log("NetworkData in store",networkData)
+// useNetworkStore.getState().setNetworkData(networkData);
+
+
 import { create } from "zustand";
 import { NetworkData } from "../types/types";
 import networkData from "../data/networkData";
+import getNetworkData from "../utils/fetchNetworkData";
 
 interface NetworkState {
     networkData: NetworkData;
@@ -13,7 +35,12 @@ export const useNetworkStore = create<NetworkState>((set) => ({
       connections: [],
       groups: []
     },
-    setNetworkData: (data: NetworkData) => set({ networkData: data }),
+    setNetworkData: async () => {
+      const data = await getNetworkData();
+      if (data) {
+        set({ networkData: data });
+      }
+    },
 }));
 
 useNetworkStore.getState().setNetworkData(networkData);
