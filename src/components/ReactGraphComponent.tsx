f957c91db4042723af06cffcedfc3c5afb8e5288
kalpesh0487 <desaikalpesh2003@gmail.com>
2025-04-24 10:18:25 +0530

    Form Added and backend fetching

--- src/components/ReactGraphComponent.tsx
+++ src/components/ReactGraphComponent.tsx
@@ -16,6 +16,7 @@ import { useNetworkStore } from '../store/dataStore';
 import useSideBarStore from '../store/sideBarStore';
 import { useDeviceSelectionStore } from '../store/deviceSelectionStore';
 import { DeviceType } from '../types/types';
+import Form from './Form';
 
 export function ReactGraphComponent() {
   const graphComponentContainer = useRef<HTMLDivElement>(null);
@@ -210,6 +211,9 @@ export function ReactGraphComponent() {
       <div className="graph-component-container " ref={graphComponentContainer} />
       <div className="absolute bottom-4 left-4">
         <Inventory />
+      </div>
+      <div className="absolute top-4 left-4">
+        <Form/>
       </div>
     </div>
   );
